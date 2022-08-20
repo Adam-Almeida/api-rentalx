b43c2933e672a337f7952bed7c59d2a5fdd124c0
adam-almeida <adamportifolio@gmail.com>
2022-08-19 22:20:36 -0300

    add uuid lib

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
-import express from 'express'
+import express from "express"
+import { categoriesRoutes } from "./routes/categories.routes"
 const port = process.env.PORT || 3333
 
 const app = express()
+
+app.use(express.json())
+
+app.use(categoriesRoutes)
 
 app.listen(port, () => console.log(`Server is running on ${port}`))
