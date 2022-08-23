3c6fb04cc60ac9577453553267f685270e47354d
adam-almeida <adamportifolio@gmail.com>
2022-08-22 22:28:44 -0300

    refatorando list  useCases de Categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
-import { Request, Response, Router } from "express"
-import { CategoriesRespository } from "../modules/cars/repositories/CategoriesRespository"
+import { Router } from "express"
 import { createCategoryController } from "../modules/cars/useCases/createCategory"
+import { listCategoriesController } from "../modules/cars/useCases/listCategories"
 
 const categoriesRoutes = Router()
-const categoriesRespository = new CategoriesRespository()
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res)
 })
 
-categoriesRoutes.get("/", (request: Request, response: Response) => {
-    const all = categoriesRespository.list()
-
-    return response.json(all)
+categoriesRoutes.get("/", (req, res) => {
+    return listCategoriesController.handle(req, res)
 })
 
 export { categoriesRoutes }
